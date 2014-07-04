1af94b1ec7ee21b543791f4034b07be2d8994353
Krzysztof Krupski <mrkrupski@users.noreply.github.com>
2014-07-04 12:08:24 +0200

    Update rack-affiliates.rb
    remove `aff_tag`

--- lib/rack-affiliates.rb
+++ lib/rack-affiliates.rb
@@ -5,13 +5,11 @@ module Rack
   # +env['affiliate.time'] if it detects a request came from an affiliated link 
   #
   class Affiliates
-    COOKIE_TAG = "aff_tag"
     COOKIE_FROM = "aff_from"
     COOKIE_TIME = "aff_time"
 
     def initialize(app, opts = {})
       @app = app
-      @param = opts[:param] || "ref"
       @cookie_ttl = opts[:ttl] || 60*60*24*30  # 30 days
       @cookie_domain = opts[:domain] || nil
       @allow_overwrite = opts[:overwrite].nil? ? true : opts[:overwrite] 
@@ -20,34 +18,24 @@ module Rack
     def call(env)
       req = Rack::Request.new(env)
 
-      params_tag = req.params[@param]
-      cookie_tag = req.cookies[COOKIE_TAG]
+      from, time = cookie_info(req)
 
-      if cookie_tag
-        tag, from, time = cookie_info(req)
-      end
-
-      if params_tag && params_tag != cookie_tag
-        if tag
-          if @allow_overwrite
-            tag, from, time = params_info(req)
-          end
-        else
-          tag, from, time = params_info(req)
+      if from
+        if @allow_overwrite
+          from, time = params_info(req)
         end
+      else
+        from, time = params_info(req)
       end
 
-      if tag
-        env["affiliate.tag"] = tag
+      if from
         env['affiliate.from'] = from
         env['affiliate.time'] = time
       end
 
       status, headers, body = @app.call(env)
 
-      if tag != cookie_tag
-        bake_cookies(headers, tag, from, time)
-      end
+      bake_cookies(headers, from, time)
 
       [status, headers, body]
     end
@@ -61,14 +49,13 @@ module Rack
     end
 
     def cookie_info(req)
-      [req.cookies[COOKIE_TAG], req.cookies[COOKIE_FROM], req.cookies[COOKIE_TIME].to_i] 
+      [req.cookies[COOKIE_FROM], req.cookies[COOKIE_TIME].to_i] 
     end
 
     protected
-    def bake_cookies(headers, tag, from, time)
+    def bake_cookies(headers, from, time)
       expires = Time.now + @cookie_ttl
-      { COOKIE_TAG => tag, 
-        COOKIE_FROM => from, 
+      { COOKIE_FROM => from, 
         COOKIE_TIME => time }.each do |key, value|
           cookie_hash = {:value => value, :expires => expires}
           cookie_hash[:domain] = @cookie_domain if @cookie_domain
